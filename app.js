78dcd802c41ae5f6b42722fb7cec7d12e3aaf01c
root <root@DESKTOP-4I5C2KQ.localdomain>
2020-09-21 16:28:42 -0500

    Corrección

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 
-const userAgent = require('express-useragent')
+// const userAgent = require('express-useragent')
 
-app.use(userAgent.express())
+// app.use(userAgent.express())
 app.get('/', (req, res) => {
-  res.send(req.useragent.browser)
+  res.send(req.get('User-Agent'))
 })
 
 app.listen(3000, () => console.log('Listen on  port 3000!'))
